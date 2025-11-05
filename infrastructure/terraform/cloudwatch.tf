a6c2006a081408a4edcf5e1646cadb98f223ce33
Faisal Momoniat <faisalmomoniat@gmail.com>
2025-11-05 16:06:35 +0000

    modifying cloudwatch tf config instructions

--- infrastructure/terraform/cloudwatch.tf
+++ infrastructure/terraform/cloudwatch.tf
@@ -1,1 +1,6 @@
 # TODO: CloudWatch Log Group for lambda function
+# sns topic for alerts
+# sns subscription
+# cloudwatch alarm for lambda errors
+# cloudwatch alarm for glue job failures
+# cloudwatch alarm for step function execution failure

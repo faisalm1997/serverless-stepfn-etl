371482414b1e913fe6d380f32cf165df5f4c1028
Faisal Momoniat <faisalmomoniat@gmail.com>
2025-11-13 12:58:40 +0000

    adding main.tf config

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,4 +1,15 @@
-# TODO: Add main config
-# Add terraform version 
-# Add terraform provider
-# Get current AWS account info using data blocks / locals
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+locals {
+  account_id  = data.aws_caller_identity.current.account_id
+  region      = data.aws_region.current.name
+  name_prefix = "${var.project_name}-${var.environment}"
+
+  common_tags = {
+    Project     = var.project_name
+    Environment = var.environment
+    ManagedBy   = "Terragrunt"
+    Terraform   = "true"
+  }
+}

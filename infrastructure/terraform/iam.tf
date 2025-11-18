623c2fc069357db3a0acf80ba6d51da7507c7270
Faisal Momoniat <faisalmomoniat@gmail.com>
2025-11-18 11:41:43 +0000

    changing main.tf to remove locals, adding iam.tf config for stpfn, eventbridge, lambda, glue

--- infrastructure/terraform/iam.tf
+++ infrastructure/terraform/iam.tf
@@ -14,11 +14,11 @@ data "aws_iam_policy_document" "lambda_assume_role" {
 
 # Lambda execution role
 resource "aws_iam_role" "lambda_execution" {
-  name               = "${var.lambda_function_name}-role"
+  name               = "${var.lambda_function_name}-lambda-role"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
 
   tags = merge(var.common_tags, {
-    Name = "${var.lambda_function_name}-role"
+    Name = "${var.lambda_function_name}-lambda-role"
   })
 }
 
@@ -40,7 +40,11 @@ resource "aws_iam_role_policy" "lambda_s3_cloudwatch_policy" {
         ]
         Resource = [
           aws_s3_bucket.raw_bucket.arn,
-          "${aws_s3_bucket.raw_bucket.arn}/*"
+          "${aws_s3_bucket.raw_bucket.arn}/*",
+          aws_s3_bucket.curated_bucket.arn,
+            "${aws_s3_bucket.curated_bucket.arn}/*",
+            aws_s3_bucket.scripts_bucket.arn,
+            "${aws_s3_bucket.scripts_bucket.arn}/*"
         ]
       },
       {
@@ -50,7 +54,7 @@ resource "aws_iam_role_policy" "lambda_s3_cloudwatch_policy" {
           "logs:CreateLogStream",
           "logs:PutLogEvents"
         ]
-        Resource = "arn:aws:logs:${var.aws_region}:${local.account_id}:log-group:/aws/lambda/*"
+        Resource = "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/*"
       }
     ]
   })
@@ -58,8 +62,191 @@ resource "aws_iam_role_policy" "lambda_s3_cloudwatch_policy" {
 
 # Create glue execution role, needs S3 read/write, Glue catalog access
 
+data "aws_iam_policy_document" "glue_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["glue.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "glue_execution" {
+  name               = "${var.project_name}-${var.environment}-glue-role"
+  assume_role_policy = data.aws_iam_policy_document.glue_assume_role.json
+
+  tags = merge(var.common_tags, {
+    Name = "${var.project_name}-${var.environment}-glue-role"
+  })
+}
+
+resource "aws_iam_role_policy" "glue_s3_catalog_policy" {
+  name = "glue-s3-catalog-policy"
+  role = aws_iam_role.glue_execution_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucket",
+        "s3:DeleteObject"
+        ]
+        Resource = [
+          aws_s3_bucket.raw_bucket.arn,
+          "${aws_s3_bucket.raw_bucket.arn}/*",
+          aws_s3_bucket.curated_bucket.arn,
+          "${aws_s3_bucket.curated_bucket.arn}/*",
+          aws_s3_bucket.scripts_bucket.arn,
+            "${aws_s3_bucket.scripts_bucket.arn}/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "glue:*"
+        ]
+        Resource = [
+          "arn:aws:glue:${var.aws_region}:${data.aws_caller_identity.current.account_id}:catalog",
+          "arn:aws:glue:${var.aws_region}:${data.aws_caller_identity.current.account_id}:database/*",
+          "arn:aws:glue:${var.aws_region}:${data.aws_caller_identity.current.account_id}:table/*/*"
+        ]
+      },
+          {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:log-group:/aws-glue/*"
+      }
+    ]
+  })
+}
 
 # Create step function execution role, needs lambda invoke/glue start job permissions
-# Create IAM role for eventbridge to invoke step functions
-# Create IAM policy to allow starting Step Functions execution
+data "aws_iam_policy_document" "stp_fn_assume_role" {
+  statement {
+    effect = "Allow"
 
+    principals {
+      type        = "Service"
+      identifiers = ["states.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "stp_fn_execution" {
+  name               = "${var.project_name}-${var.environment}-stepfn-role"
+  assume_role_policy = data.aws_iam_policy_document.stp_fn_assume_role.json
+
+  tags = merge(var.common_tags, {
+    Name = "${var.project_name}-${var.environment}-stepfn-role"
+  })
+}
+
+resource "aws_iam_role_policy" "stepfn_lambda_glue_policy" {
+  name = "stepfn-lambda-glue-policy"
+  role = aws_iam_role.stp_fn_execution_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "lambda:InvokeFunction"
+        ]
+        Resource = aws_lambda_function.etl_lambda.arn
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "glue:StartJobRun",
+          "glue:GetJobRun",
+          "glue:GetJobRuns",
+            "glue:BatchStopJobRun"
+        ]
+        Resource = aws_glue_job.etl_glue_job.arn
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "glue:StartCrawler",
+          "glue:GetCrawler"
+        ]
+        Resource = [
+          "arn:aws:glue:${var.aws_region}:${data.aws_caller_identity.current.account_id}:crawler/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogDelivery",
+          "logs:GetLogDelivery",
+          "logs:UpdateLogDelivery",
+          "logs:DeleteLogDelivery",
+          "logs:ListLogDeliveries",
+          "logs:PutResourcePolicy",
+          "logs:DescribeResourcePolicies",
+          "logs:DescribeLogGroups"
+        ]
+        "*"
+      }
+    ]
+  })
+}
+
+# Create IAM role for eventbridge to invoke step functions
+
+data "aws_iam_policy_document" "eventbridge_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "eventbridge_execution" {
+  name               = "${var.project_name}-${var.environment}-eventbridge-role"
+  assume_role_policy = data.aws_iam_policy_document.eventbridge_assume_role.json
+
+  tags = merge(var.common_tags, {
+    Name = "${var.project_name}-${var.environment}-eventbridge-role"
+  })
+}
+
+# Create eventbridge IAM policy to allow starting Step Functions execution
+
+resource "aws_iam_role_policy" "eventbridge_stepfn_policy" {
+  name = "eventbridge-stepfn-policy"
+  role = aws_iam_role.eventbridge_execution_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "states:StartExecution"
+        ]
+        Resource = aws_sfn_state_machine.stepfn_etl.arn
+      }
+    ]
+  })
+}

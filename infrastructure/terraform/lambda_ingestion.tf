0a3818cc88cfa737e09ba6a2011805240fe0782f
Faisal Momoniat <faisalmomoniat@gmail.com>
2025-11-18 12:15:48 +0000

    adding lambda config to tf setup

--- infrastructure/terraform/lambda_ingestion.tf
+++ infrastructure/terraform/lambda_ingestion.tf
@@ -1,2 +1,32 @@
-# TODO: Create Lambda function for API ingestion
-# Add dependency to pull filename from src/lambda after script has been run
+# Package Lambda code automatically
+
+data "lambda_package" "lambda_ingestion_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/../../src/lambda/ingestion"
+  output_path = "${path.module}/../../src/lambda/lambda_ingestion.zip"
+}
+
+# Lambda function
+
+resource "aws_lambda_function" "lambda_ingestion" {
+  function_name    = "${var.project_name}-${var.environment}-lambda-ingestion"
+  role             = aws_iam_role.lambda_execution.arn
+  handler          = "ingestion_handler.lambda_handler"
+  runtime          = "python3.12"
+  filename         = data.lambda_package.lambda_ingestion_zip.output_path
+  source_code_hash = data.lambda_package.lambda_ingestion_zip.output_base64sha256
+
+  environment {
+    variables = {
+      RAW_BUCKET_NAME = aws_s3_bucket.raw_bucket.bucket
+    #   API_ENDPOINT    = var.api_endpoint
+    }
+  }
+
+  timeout     = 300
+  memory_size = 512
+
+  tags = merge(var.common_tags, {
+    Name = "${var.project_name}-${var.environment}-lambda-ingestion"
+  })
+}
